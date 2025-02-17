acc59321570e731fdc3a1636d3bae7dda77ea2b8
Alessio Filippucci <alessio.filippucci@digitware.it>
2025-02-17 11:15:22 +0100

    Add sort list code

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -5,18 +5,28 @@
         static void Main(string[] args)
         {
             // default constructor creates an empty list
-            List<int> list = new List<int>();
+            var list = new List<int> { 1, 45, 78, 56 };
             list.Add(10);
             list.Add(20);
+            
             Console.WriteLine("Default Constructor: ");
+            
             foreach (var item in list)
             {
                 Console.WriteLine(item);
             }
 
+            string.Join(",", list);
+
+            //for (var i = 0; i < list.Count; i++)
+            //{
+            //    Console.WriteLine(list[i]);
+            //    Console.WriteLine(list.ElementAt(i));
+            //}
+
             // Construnctors from IEnumerable
             int[] num = { 10, 20 };
-            List<int> enumerableList = new List<int>(num);
+            var enumerableList = new List<int>(num);
             Console.WriteLine("Constructor with IEnumerable: ");
             foreach (var item in enumerableList)
             {
@@ -24,7 +34,7 @@
             }
 
             // Constructor with Initial Capacity 
-            List<int> cList = new List<int>(2);
+            var cList = new List<int>(2);
             cList.Add(10);
             cList.Add(20);
             Console.WriteLine("Constructor with Initial Capacity: ");
---
+++ SortList/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SortList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            var input = "";
+
+            do
+            {
+                Console.WriteLine("Inserisci un numero o q per uscire");
+                input = Console.ReadLine();
+
+                if (int.TryParse(input, out int number))
+                {
+                    numbers.Add(number);
+                }
+            }
+            while (input != "q");
+
+            numbers.Sort();
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
