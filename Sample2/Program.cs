02375777edb20e7575a5661e369df9a430a34b1e
Alessio Filippucci <alessio.filippucci@digitware.it>
2025-02-18 10:27:46 +0100

    Add Sample2

---
+++ Sample2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Sample2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string _firstName = GetFirstName();
+            string _lastName = GetLastName();
+
+            PrintFullName(_firstName, _lastName);
+        }
+
+        static string GetFirstName()
+        {
+
+        }
+        static string GetLastName()
+        {
+
+        }
+
+        static void PrintFullName(string firstName, string lastName)
+        {
+            Console.WriteLine($"{firstName} {lastName}");
+        }
+    }
+}
