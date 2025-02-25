c2aa9c84a49ce3c00a580f4080111864bea46d7e
Alessio Filippucci <alessio.filippucci@digitware.it>
2025-02-25 16:18:46 +0100

    Add base calculator

--- Sample4/Program.cs
+++ Sample4/Program.cs
@@ -4,20 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Person pers = new Person();
-
-            pers.FirstName = "Alessio";
-            pers.LastName = "Filippucci";
-            pers.Dob = new DateTime(1989, 6, 11);
-
-            pers.DisplayFullname();
-
-            var age = pers.GetAge();
-            Console.WriteLine($"La tua età è di {age} anni");
-
-
-            var pers1 = new Person("Alessio", "Filippucci", new DateTime(1989, 6, 11));
-
+            var cal = new Calculator();
+            cal.Start();
         }
     }
 }
