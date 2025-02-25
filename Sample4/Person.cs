eb323ccec9709ee580239ecf5842dcc469a1960a
Alessio Filippucci <alessio.filippucci@digitware.it>
2025-02-25 16:14:55 +0100

    Add Sample4

---
+++ Sample4/Person.cs
@@ -0,0 +1,48 @@
+﻿namespace Sample4
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public DateTime Dob;
+
+        public Person()
+        {
+        }
+
+        public Person(string firstName, string lastName, DateTime dob)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Dob = dob;
+        }
+
+        public int GetAge()
+        {
+            var age = DateTime.Now.Year - Dob.Year;
+
+            if (DateTime.Now > Dob)
+            {
+                age--;
+            }
+
+            return age;
+        }
+
+        public void DisplayFullname()
+        {
+            Console.WriteLine($"{FirstName} {LastName}");
+        }
+
+        public void DisplayFirstName()
+        {
+            Console.WriteLine($"{FirstName}");
+        }
+
+        public void DisplayLastName()
+        {
+            Console.WriteLine($"{LastName}");
+        }
+
+    }
+}
---
+++ Sample4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Sample4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person pers = new Person();
+
+            pers.FirstName = "Alessio";
+            pers.LastName = "Filippucci";
+            pers.Dob = new DateTime(1989, 6, 11);
+
+            pers.DisplayFullname();
+
+            var age = pers.GetAge();
+            Console.WriteLine($"La tua età è di {age} anni");
+
+
+            var pers1 = new Person("Alessio", "Filippucci", new DateTime(1989, 6, 11));
+
+        }
+    }
+}
